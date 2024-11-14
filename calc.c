9f5cf3f77d18fb4d4a0123d207bec78e129e1535
SatanaKILLER666 <mrtgolova@outlook.com>
2024-11-14 03:42:05 +0300

    Реализация функции подсчета результата, считывание аргументов с командной строки и работа с ними

--- calc.c
+++ calc.c
@@ -1,3 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include"calc.h"
+
+//Функция для вычисления результата
+
+int calc(int l_op, char znak, int r_op){
+    switch(znak){
+        case '+':
+            return l_op + r_op;
+        case '-':
+            return l_op - r_op;
+        case '*':
+            return l_op * r_op;
+        case '%':
+            if(r_op == 0){
+                printf("Деление на ноль\n");
+                exit(1);
+            }
+            return l_op % r_op;
+        default:
+            printf("Введён некорректный оператор\n");
+            exit(2);
+    }
+}
+
+//Функция преобразования результирующей текстовой строки
+
+char decrypt(int res, int k) { 
+    return (char)(res - k); 
+}
--- calc.h
+++ calc.h
@@ -1,2 +1,14 @@
 #ifndef CALC_H
 #define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int calc(int l_op, char znak, int r_op); //Функция для вычисления результата
+
+//Функция преобразования результирующей текстовой строки
+
+char decrypt(int res, int k); //Функция преобразования результирующей текстовой строки
+
+#endif
